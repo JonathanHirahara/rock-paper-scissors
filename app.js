9cb996fca9b074b8c841c7297919cb16d1edc7c6
JonathanHirahara <jdhirahara@gmail.com>
2019-06-13 14:57:47 -0600

    bleh

--- app.js
+++ app.js
@@ -39,23 +39,3 @@ function play(player) {
   }
 
 }
-/*
-
-responsible for taking the values of the cat object and printing them onto the page
-function drawCat() {
-  let index = Math.floor((Math.random() * 3) + 1)
-  if (cat.petCount == 0) {
-    index = 0
-  }
-  let moodObj = cat.moods[index]
-
-
-  let nameElem = document.querySelector("#cat-name")
-  let moodElem = document.querySelector("#cat-mood")
-  let petCountElem = document.querySelector("#cat-pet-count")
-  let imageElem = document.querySelector("#cat-image")
-  nameElem.innerHTML = cat.name
-  moodElem.innerHTML = moodObj.mood
-  petCountElem.innerHTML = cat.petCount.toString()
-  imageElem.src = moodObj.image
-}*/
